ac3d2ac3be89a43e44b417db3bc5bb73cf877aa6
heathercook77 <82852003+heathercook77@users.noreply.github.com>
2021-11-09 06:33:33 -0600

    Update SurveyForm.js

--- src/components/SurveyForm/SurveyForm.js
+++ src/components/SurveyForm/SurveyForm.js
@@ -14,19 +14,23 @@ export default function SurveyForm() {
       <h1>Survey Form</h1>
       <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Label>
+            Email address<span className="required-ast">*</span>
+          </Form.Label>
+          <Form.Control type="email" placeholder="Enter email" required />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+        <Form.Group className="mb-3" controlId="formBasicPassword" required>
+          <Form.Label>
+            Password<span className="required-ast">*</span>
+          </Form.Label>
+          <Form.Control type="password" required placeholder="Password" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>What is your preferred cuisine?</Form.Label>
+          <Form.Label>What is your preferred cuisine? (Optional)</Form.Label>
           <Form.Control
             type="input"
             placeholder="Please enter your preferred cuisine..."
@@ -36,6 +40,7 @@ export default function SurveyForm() {
           <Form.Check type="checkbox" label="Check me out" />
         </Form.Group>
         <Button variant="primary" type="submit" onClick={handleSave}>
+        
           Save Data
         </Button>
       </Form>
